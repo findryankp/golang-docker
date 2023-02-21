6457c7801ff0a88fb55114c4476a3af963353dfe
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2023-02-21 14:27:04 +0700

    param, query, form, bind

--- 10-intro-api/echo-server/main.go
+++ 10-intro-api/echo-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,8 +10,16 @@ import (
 // go get -u github.com/labstack/echo/v4
 
 type User struct {
+	Id    int    `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
+}
+
+type Article struct {
+	Id          int    `json:"id" form:"id"`
+	Title       string `json:"title" form:"title"`
+	Writer      string `json:"writer" form:"writer"`
+	Description string `json:"description" form:"description"`
 }
 
 func main() {
@@ -24,7 +33,15 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World! POST")
 	})
 
+	// query param
 	e.GET("/users", GetUsersController)
+	// param
+	e.GET("/users/:id", GetUserByIdController)
+	// form value
+	e.POST("/users", PostUserController)
+	e.PUT("/users", PostUserController)
+	// bind
+	e.POST("/articles", PostArticleController)
 	e.Logger.Fatal(e.Start(":80"))
 }
 
@@ -36,10 +53,78 @@ func GetArticles(c echo.Context) error {
 	})
 }
 
+//query param
 func GetUsersController(c echo.Context) error {
+	nameQuery := c.QueryParam("name")
+	pageQuery := c.QueryParam("page")
+
+	var data = []User{
+		{1, "Alta", "alta@alterra.id"},
+		{2, "Alta", "alta@alterra.id"},
+		{3, "Alta", "alta@alterra.id"},
+		{4, "Alta", "alta@alterra.id"},
+	}
+	// return c.JSON(http.StatusOK, data)
+	return c.JSON(http.StatusOK, map[string]any{
+		"message": "success",
+		"query":   nameQuery,
+		"page":    pageQuery,
+		"data":    data,
+	})
+}
+
+//param
+func GetUserByIdController(c echo.Context) error {
+	id := c.Param("id")
+	idConv, err := strconv.Atoi(id)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]any{
+			"message": "id harus angka",
+		})
+	}
+
 	var data = User{
+		Id:    idConv,
 		Name:  "Alta",
 		Email: "alta@alterra.id",
 	}
 	return c.JSON(http.StatusOK, data)
+}
+
+// request body dengan pendekatan form value
+func PostUserController(c echo.Context) error {
+	nameForm := c.FormValue("name")
+	emailForm := c.FormValue("email")
+
+	var data = User{
+		Id:    1,
+		Name:  nameForm,
+		Email: emailForm,
+	}
+	return c.JSON(http.StatusOK, map[string]any{
+		"message": "success",
+		"data":    data,
+	})
+}
+
+// request body dengan pendekatan bind
+func PostArticleController(c echo.Context) error {
+	// binding data
+	articleInput := Article{}
+	errBind := c.Bind(&articleInput)
+	if errBind != nil {
+		return c.JSON(http.StatusBadRequest, map[string]any{
+			"message": "error bind data",
+		})
+	}
+	if articleInput.Title == "" {
+		return c.JSON(http.StatusBadRequest, map[string]any{
+			"message": "title tidak boleh kosong",
+		})
+	}
+	//  c.Request().FormFile("file")
+	return c.JSON(http.StatusOK, map[string]any{
+		"message": "success",
+		"data":    articleInput,
+	})
 }
