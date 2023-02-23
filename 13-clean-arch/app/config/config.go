b78a8e8e56b27ba57a3a59ea1f7f751acfaafdff
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2023-02-23 15:01:58 +0700

    [wip] clean arch

---
+++ 13-clean-arch/app/config/config.go
@@ -0,0 +1,77 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	SECRET_JWT string = ""
+)
+
+type AppConfig struct {
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_HOSTNAME string
+	DB_PORT     int
+	DB_NAME     string
+	jwtKey      string
+}
+
+func InitConfig() *AppConfig {
+	return ReadEnv()
+}
+
+func ReadEnv() *AppConfig {
+	app := AppConfig{}
+	isRead := true
+
+	if val, found := os.LookupEnv("JWT_KEY"); found {
+		app.jwtKey = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBUSER"); found {
+		app.DB_USERNAME = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPASS"); found {
+		app.DB_PASSWORD = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBHOST"); found {
+		app.DB_HOSTNAME = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPORT"); found {
+		cnv, _ := strconv.Atoi(val)
+		app.DB_PORT = cnv
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBNAME"); found {
+		app.DB_NAME = val
+		isRead = false
+	}
+
+	if isRead {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("local")
+		viper.SetConfigType("env")
+
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Println("error read config : ", err.Error())
+			return nil
+		}
+		err = viper.Unmarshal(&app)
+		if err != nil {
+			log.Println("error parse config : ", err.Error())
+			return nil
+		}
+	}
+
+	SECRET_JWT = app.jwtKey
+	return &app
+}
